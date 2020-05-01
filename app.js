6f9dfc7d84e4dbaf782c60fba0bef01752336e93
Winnie Kelley <alldayallnight642@icloud.com>
2020-05-01 12:33:08 -0400

    basically everything seem to be working except for error handling for meets expectation, still working on styling for exceeds

---
+++ app.js
@@ -0,0 +1,34 @@
+// Add variables to require the necessary dependencies.
+const express = require('express');
+const app = express();
+const {projects} = require('./data.json');
+
+
+//set “view engine” to “pug”
+app.set('view engine', 'pug');
+
+//use a static route and the express.static method to serve the static files located in the public folder
+app.use('/static', express.static('public'));
+
+//set routes:
+//index:
+app.get('/', (req, res) =>{
+    res.render('index', {projects});    
+});
+
+//about:
+app.get('/about', (req, res) =>{
+    res.render('about', {projects});
+});
+
+//Dynamic "project" routes (/project or /projects) based on the id of the project that render a customized version of the Pug project template to show off each project. Which means adding data, or "locals", as an object that contains data to be passed to the Pug template.
+app.get('/project/:id', (req, res) =>{
+    res.render('project',{
+        project: projects[req.params.id]
+    })    
+})
+app.get('/', (request, respond) =>{
+    respond.send('connected properly to localhost:3000');
+});
+
+app.listen(3000);
